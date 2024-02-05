e70a7a08208e0f40198e348aa2b7208b39b68a27
Hamza <jhamzaahmed@gmail.com>
2024-02-05 11:58:27 +0500

    added more js

--- app.js
+++ app.js
@@ -7,12 +7,30 @@ const rightArrow = document.querySelector(".arrow-right");
 const leftSlider = document.querySelector(".left-slider");
 const rightSlider = document.querySelector(".right-slider");
 const activeSlider = document.querySelector(".active-slider");
+const rightNext = document.querySelector(".right-next");
 
 console.log(leftSlider);
 
 leftArrow.addEventListener("click", () => {
-  leftSlider.classList.add("left-un-active");
+  leftSlider.style.position = "absolute";
+  leftSlider.style.translate = "-150%";
+  leftSlider.classList.replace("left-slider", "left-next");
+
   activeSlider.classList.add("center-to-left");
-  activeSlider.classList.replace("active-slider", "left-slider");
-  rightSlider.classList.replace("right-slider", "active-slider");
+  setTimeout(() => {
+    activeSlider.classList.remove("center-to-left");
+    activeSlider.classList.replace("active-slider", "left-slider");
+  }, 300);
+
+  rightSlider.style.translate = "-10%";
+  setTimeout(() => {
+    rightSlider.classList.replace("right-slider", "active-slider");
+    rightSlider.style.translate = "0";
+  }, 300);
+
+  rightNext.style.position = "relative";
+  rightNext.style.transform = "translateX(0)";
+  setTimeout(() => {
+    activeSlider.classList.remove("right-next");
+  }, 300);
 });
