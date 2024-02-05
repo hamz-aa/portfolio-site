1b6b0632cf6016f13d463173c03e10731a5c0b51
Hamza <jhamzaahmed@gmail.com>
2024-02-05 16:12:15 +0500

    updated app.js

--- app.js
+++ app.js
@@ -4,12 +4,16 @@
 const leftArrow = document.querySelector(".arrow-left");
 const rightArrow = document.querySelector(".arrow-right");
 
-const leftSlider = document.querySelector(".left-slider");
-const rightSlider = document.querySelector(".right-slider");
-const activeSlider = document.querySelector(".active-slider");
-const rightNext = document.querySelector(".right-next");
+let leftSlider = document.querySelector(".left-slider");
+let rightSlider = document.querySelector(".right-slider");
+let activeSlider = document.querySelector(".active-slider");
+const projects = document.querySelector(".projects-wrapper");
 
-console.log(leftSlider);
+let left = leftSlider.previousElementSibling;
+let right = rightSlider.nextElementSibling;
+
+adjustLeft();
+adjustRight();
 
 leftArrow.addEventListener("click", () => {
   leftSlider.style.position = "absolute";
@@ -28,9 +32,51 @@ leftArrow.addEventListener("click", () => {
     rightSlider.style.translate = "0";
   }, 300);
 
-  rightNext.style.position = "relative";
-  rightNext.style.transform = "translateX(0)";
+  right.style.position = "relative";
+  right.style.transform = "translateX(0)";
   setTimeout(() => {
-    activeSlider.classList.remove("right-next");
+    right.classList.replace("right-next", "right-slider");
+
+    leftSlider = document.querySelector(".left-slider");
+    rightSlider = document.querySelector(".right-slider");
+    activeSlider = document.querySelector(".active-slider");
+
+    left.style.display = "none";
+    left = leftSlider.previousElementSibling;
+    adjustLeft();
+
+    right = rightSlider.nextElementSibling;
+    adjustRight();
   }, 300);
 });
+
+function adjustLeft() {
+  if (left.nodeName === "BUTTON") {
+    const lastElement = projects.lastElementChild.previousElementSibling;
+    projects.insertBefore(lastElement, leftSlider);
+    left = lastElement;
+  }
+
+  if (left.classList.contains("right-slider"))
+    left.classList.remove("right-slider");
+  if (!left.classList.contains("left-next")) left.classList.add("left-next");
+  left.style.display = "block";
+  left.style.position = "absolute";
+  left.style.transform = "translateX(-150%)";
+}
+
+function adjustRight() {
+  if (right.nodeName === "BUTTON") {
+    const firstElement = projects.firstElementChild.nextElementSibling;
+    projects.insertBefore(firstElement, right);
+    right = firstElement;
+  }
+
+  if (right.classList.contains("left-slider"))
+    left.classList.remove("left-slider");
+  if (!right.classList.contains("right-next"))
+    right.classList.add("right-next");
+  right.style.display = "block";
+  right.style.position = "absolute";
+  right.style.transform = "translateX(150%)";
+}
