a6531a8570664c468a5992f8b50c4909a3ed4047
Hamza <jhamzaahmed@gmail.com>
2024-02-10 19:23:01 +0500

    adding scroll animation

--- app.js
+++ app.js
@@ -187,3 +187,18 @@ let typed = new Typed(".text-1", {
   backSpeed: 90,
   loop: true
 })
+
+// intersection observer
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if(entry.isIntersecting) {
+      entry.target.classList.add('show');
+    } else {
+      entry.target.classList.remove('show');
+    }
+  })
+})
+
+const sections = document.querySelectorAll('section');
+sections.forEach(el => observer.observe(el));
